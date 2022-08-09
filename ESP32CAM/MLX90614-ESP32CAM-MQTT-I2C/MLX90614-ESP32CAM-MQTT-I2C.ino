6a236980c7d994389cbcbd6d201a86cae5feb91f
JoErnesto <jernesto.calvillo@gmail.com>
2022-08-09 15:34:29 -0700

    Cambio de red
    Se cambió la información de red para poder conectarlo a mi modem.

--- ESP32CAM/MLX90614-ESP32CAM-MQTT-I2C/MLX90614-ESP32CAM-MQTT-I2C.ino
+++ ESP32CAM/MLX90614-ESP32CAM-MQTT-I2C/MLX90614-ESP32CAM-MQTT-I2C.ino
@@ -21,8 +21,8 @@
 #include <Wire.h>
 
 //Datos de WiFi
-const char* ssid = "AXTEL XTREMO-18D6";  // Aquí debes poner el nombre de tu red
-const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu red
+const char* ssid = "INFINITUM29A4_2.4";  // Aquí debes poner el nombre de tu red
+const char* password = "2gPXRJ28bg";  // Aquí debes poner la contraseña de tu red
 
 //Datos del broker MQTT
 //const char* mqtt_server = "192.168.15.27"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
